fc2a19bedbf2d1f035829c4067bd30e852f38c41
solygambas <solygambas@gmail.com>
2021-01-11 19:00:50 +0100

    add finished image shrink

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ const imagemin = require("imagemin");
 const imageminMozjpeg = require("imagemin-mozjpeg");
 const imageminPngquant = require("imagemin-pngquant");
 const slash = require("slash");
+const log = require("electron-log");
 
 // Set env
-process.env.NODE_ENV = "development";
+process.env.NODE_ENV = "production"; // "development"
 
 const isDev = process.env.NODE_ENV !== "production" ? true : false;
 const isMac = process.env.NODE_ENV === "darwin" ? true : false;
@@ -115,11 +116,11 @@ const shrinkImage = async ({ imgPath, quality, dest }) => {
         }),
       ],
     });
-    console.log(files);
+    log.info(files);
     shell.openPath(dest);
     mainWindow.webContents.send("image:done");
   } catch (error) {
-    console.log(error);
+    log.error(error);
   }
 };
 
